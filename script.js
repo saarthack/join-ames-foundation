933a740443719db3a7b824c973a9eeab50ae6bcc
saarthack <ssarthak2003@outlook.com>
2023-09-08 18:28:27 +0530

    applied locomotive and made page2

--- script.js
+++ script.js
@@ -1,38 +1,90 @@
-var timer = document.querySelector("#timer h1")
-var timerButton = document.querySelector("#timer button")
+var audio = new Audio("./insect-sound.mp3");
 
-var audio = new Audio("./insect-sound.mp3")
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
 
-var grow  = 0
-var int = setInterval(function(){
-    if(grow < 90){ 
-        grow += Math.floor(Math.random()*20)
-        timer.innerHTML = grow+"%"
-    } else{
-        grow = 100
-        timer.innerHTML = grow+"%"
-        // console.log("Hey")
-        timer.style.transform = "translateY(-100%)"
-        timerButton.style.transform = "translateY(-100%)"
-        timerButton.style.opacity = "1"
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-        clearInterval(int)
-       
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+function loadingAnimation() {
+  var timer = document.querySelector("#timer h1");
+  var timerButton = document.querySelector("#timer button");
+  var grow = 0;
+  var int = setInterval(function () {
+    if (grow < 90) {
+      grow += Math.floor(Math.random() * 20);
+      timer.innerHTML = grow + "%";
+    } else {
+      grow = 100;
+      timer.innerHTML = grow + "%";
+      // console.log("Hey")
+      timer.style.transform = "translateY(-100%)";
+      timerButton.style.transform = "translateY(-100%)";
+      timerButton.style.opacity = "1";
+
+      clearInterval(int);
     }
-},Math.floor(Math.random()*300))
+  }, Math.floor(Math.random() * 300));
 
+  timerButton.addEventListener("click", function () {
+    // play audio by this line
+    // audio.play()
+    gsap.to("#page1", {
+      scale: 1,
+      duration: 0.5,
+      borderRadius: 0,
+    });
+    gsap.to("#timer", {
+      opacity: 0,
+    });
+    gsap.to("#log", {
+      opacity: 1,
+    });
+    gsap.to("#nav", {
+      top: 0,
+      opacity: 1,
+      delay: 0.5,
+      duration: 0.5,
+    });
+    gsap.to("#main", {
+      overflow: "auto",
+    });
+  });
+}
 
-timerButton.addEventListener("click",function(){
-    audio.play()
-    gsap.to("#page1",{
-        scale:1,
-        duration:0.5
-    })
-    gsap.to("#timer",{
-        opacity:0
-    })
-    gsap.to("#log",{
-        opacity:1
-    })
-})
+loadingAnimation();
+loco()
